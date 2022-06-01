c435f8aebc937e6cd1018f57813213a5fbd7fcd9
Aguleales <agustinleales@gmail.com>
2022-06-01 11:25:53 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = null;
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = "holi";
+const nuevoNum = "puta";
 
 // Crea una variable booleana:
 const nuevoBool = null;
